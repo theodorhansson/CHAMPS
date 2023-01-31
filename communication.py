4dae933f3ece7762edd273f0c18f22d452a01530
Jacob Billvén <billven@chalmers.se>
2023-01-31 11:52:07 +0100

    communication switching depending on desired DC supply

--- communication.py
+++ communication.py
@@ -1,5 +1,17 @@
-def get_DCsupply():
-    pass
+from equipment import *
+
+
+def get_DCsupply(config_dict):
+    DC_type = config_dict["type"]
+    match DC_type:
+        case "keithly":
+            return keithley.keithley(config_dict)
+
+        case "powercube":
+            return powercube.powercube(config_dict)
+
+        case _:
+            pass  # TODO error handling
 
 
 def get_Sphere():
