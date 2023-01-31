be46fecbe22eac20c3d8c5b8c520038b992a1bb6
Jacob Billvén <billven@chalmers.se>
2023-01-31 17:03:04 +0100

    Powercube commands entered. Not tested though

--- equipment/powercube.py
+++ equipment/powercube.py
@@ -1,12 +1,30 @@
+import serial
+
+
 class powercube:
-    def __init__(self):
-        pass
+    def __init__(self, config_dict):
+        port = str(config_dict[port])
 
-    def get_voltage():
-        pass
+        self.serial_port = serial.Serial(port)
 
-    def set_voltage_limit():
-        pass
+        self.serial_port.baudrate = 9600
+        self.serial_port.parity = serial.PARITY_NONE
+        self.serial_port.bytesize = 8
+        self.serial_port.stopbits = 0
 
-    def set_current():
-        pass
+    def get_voltage(self):
+        self.serial_port.write(b"VOUT1?")
+        ans = self.serial_port.read(1000)
+        return ans
+
+    def set_voltage_limit(self, volt):
+        self.serial_port.write(b"VSET1:" + str(volt))
+
+    def set_current(self, current):
+        self.serial_port.write(b"ISET1:" + str(current))
+
+    def set_output(self, state):
+        self.serial_port.write(b"OUT" + str(int(state)))
+
+    def close(self):
+        self.serial_port.close()
