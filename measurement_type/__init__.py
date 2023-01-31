ddff6b30e0f2c4c57a4d41afc2d3fdcef8a82ff8
Theodor Hansson <hanssontheodor2@gmail.com>
2023-01-31 16:09:50 +0100

    added basic ipv structure

--- measurement_type/__init__.py
+++ measurement_type/__init__.py
@@ -1,12 +1,13 @@
-from os import walk
-import pathlib
+# from os import walk
+# import pathlib
 
-_path = pathlib.Path(__file__).parent.resolve()
+# _path = pathlib.Path(__file__).parent.resolve()
+# modules = []
+# for _, _, filenames in walk(_path):
+#     for file in filenames:
+#         if len(file) >= 3:
+#             if file[-3:] == ".py" and file[:-3] != "__init__":
+#                 modules.append(file)
+#                 print(file[:-3])
 
-print(_path)
-
-f = []
-for _, _, filenames in walk(_path):
-    f.extend(filenames)
-    print(filenames)
-    break
+# __all__ = modules
