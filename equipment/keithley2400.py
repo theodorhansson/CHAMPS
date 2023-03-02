219a2ac3db11c56cfc9105986501cca7ffb9cd56
Theodor Hansson <hanssontheodor2@gmail.com>
2023-03-02 16:55:56 +0100

    added manual reset to keith

--- equipment/keithley2400.py
+++ equipment/keithley2400.py
@@ -186,6 +186,12 @@ def performance_test():
     Keith.set_output(False)
     Keith.close()
 
+def manual_reset():
+    config = {"gpib_address": 24, "type": "jipsdf"}
+    Keith = keithley2400(config)
+    Keith.open()
+    Keith.close()
+
 
 if __name__ == "__main__":
-    performance_test()
+    manual_reset()
