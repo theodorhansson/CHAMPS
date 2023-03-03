e5b59e9be5c06c40bd53e70d94f98e3e56f439bd
Theodor Hansson <hanssontheodor2@gmail.com>
2023-03-03 09:22:56 +0100

    removed has_key

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main(config_path):
     meas_name = config_lower["measurement"]["type"]
 
     # Code for printing
-    if config_lower["measurement"].has_key("verbose_printing"):
+    if "verbose_printing" in config_lower["measurement"].keys():
         verbose = config_lower["measurement"]["verbose_printing"]
     else:
         verbose = 0
