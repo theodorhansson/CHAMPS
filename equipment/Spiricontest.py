80d9d9800d8d413f1c9b2a334b794a9a2248ad53
Jacob Billvén <billven@chalmers.se>
2023-03-09 11:16:38 +0100

    Closing in on target!

--- equipment/Spiricontest.py
+++ equipment/Spiricontest.py
@@ -1,6 +1,7 @@
 import os, sys, clr, time
 import pythonnet
 import matplotlib.pyplot as plt
+import numpy as np
 
 clr.AddReference("..\\beamgage_drivers\\Beamgage_python_wrapper")
 import Beamgage_python_wrapper
@@ -9,13 +10,12 @@ bg_class = Beamgage_python_wrapper.Beamgage("FROM_PYTH", True)
 bg = bg_class.get_AutomatedBeamGage()
 
 
-res = bg_class.get_Data_Double()
-res_str = bg_class.get_Data_string()
-
+res = bg.ResultsPriorityFrame.DoubleData
 res = [x for x in res]
+print("len  ", len(res), "\tmax  ", np.max(res))
 
-print(dir(bg))
-print(len(res))
-print(res_str)
-time.sleep(10)
-bg.Instance.Shutdown()
+print("Getting frame info:")
+width = bg.get_FrameInfoResults().Width
+height = bg.get_FrameInfoResults().Height
+time.sleep(2)
+# bg.Instance.Shutdown()
